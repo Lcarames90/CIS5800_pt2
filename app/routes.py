ca229820aa55261d833a6e5a9963829e9cb4e4ec
Lee C <Caramesl@gmail.com>
2022-11-16 13:39:15 -0500

    base file & jinja placement

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,21 @@ from app import app
 @app.route('/index')
 def index():
     user = { 'username': 'Lee'}
-    return 
+
+    posts = [
+        {
+            'author': {'username': 'Jefferson'},
+            'body':'Table Plus & CockroachDB are foreign to me'
+        },
+        {
+            'author':{'username': 'Avinash'},
+            'body':'Lee, hurry up!'
+        },
+
+        {
+            'author':{'username': 'Lee'},
+            'body': 'Sorry Professors!'
+        },
+    ]
+
+    return render_template('index.html', title = 'Home', user=user, posts = posts)
