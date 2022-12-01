24e6e1dd1575451e0c7b6aa70389f90e44fc82a3
Lee C <Caramesl@gmail.com>
2022-12-01 10:24:09 -0500

    error fixing

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,14 @@ class EditProfileForm(FlaskForm):
     submit = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self)
+        super(EditProfileForm, self).__init(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
     
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
